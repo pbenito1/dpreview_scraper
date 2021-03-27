0bbd0845ce4149472ccc6e8735fe7ebd7f080d67
Pablo Be <superblobe@gmail.com>
2021-03-28 00:40:31 +0100

    Añadida descarga de listado de camaras

--- app.py
+++ app.py
@@ -1,39 +1,90 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 BASE_URL = "https://www.dpreview.com/products/"
 
+# Creamos una sesión de requests para reutilizar la conexión.
+s = requests.Session()
 
-def get_camera_list(pageList=None):
+
+def get_all_cameras():
+    page = 1
+    all_cameras = []
+    page_res = get_camera_list(page)
+    while page_res:
+        all_cameras = all_cameras + page_res
+        page = page+1
+        page_res = get_camera_list(page)
+        time.sleep(1)
+    return all_cameras
+
+
+def get_camera_list(pagenum=1):
     # Pablo
     # https://www.dpreview.com/products/cameras/all?view=list
     # https://www.dpreview.com/products/cameras/all?view=list&page=2
-    page = requests.get(BASE_URL+"/cameras/all?view=list")
 
-    for camera in cameraList:
-        specs = get_specs(camera)
+    camera_list = []
+    page = s.get(BASE_URL+"/cameras/all?view=list&page="+str(pagenum))
+    print("Procesando "+BASE_URL+"/cameras/all?view=list&page="+str(pagenum))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup.prettify())
+    # cameras = soup.select("tr.product")
+    cameras = soup.find_all('tr', attrs={'class': 'product'})
+    for c in cameras:
+        current_camera = {}
+        current_camera['image'] = c.td.div.a.img.get('src')
+        current_camera['name'] = c.find(
+            'div', attrs={'class': 'name'}).a.get_text()
+        current_camera['link'] = c.find(
+            'div', attrs={'class': 'name'}).a.get('href')
+
+        if c.find('div', attrs={'class': 'announcementDate'}):
+            current_camera['announcement_date'] = c.find(
+                'div', attrs={'class': 'announcementDate'}).get_text()
+
+        if c.find('div', attrs={'class': 'specs'}):
+            current_camera['quick_specs'] = c.find(
+                'div', attrs={'class': 'specs'}).get_text()
+
+        # Comprobamos si tiene review
+        if c.find('td', attrs={'class': 'review'}).get_text():
+            current_camera['review_link'] = c.find(
+                'td', attrs={'class': 'review'}).find('a').get('href')
+            current_camera['review_value'] = c.find('td', attrs={'class': 'review'}).find(
+                'span', attrs={'class': 'value'}).get_text()
+            current_camera['review_award'] = c.find('td', attrs={'class': 'review'}).find(
+                'span', attrs={'class': 'text'}).get_text()
+        camera_list.append(current_camera)
+
+    # if camera_list:
+    #    camera_list.append(get_camera_list(pagenum+1))
+
+    return camera_list
 
 
-def get_specs(camera):
+def get_specs(camera_url):
     # camera: fujifilm/slrs/fujifilm_xs10
     # Miki
     specs = {}
     # https://www.dpreview.com/products/fujifilm/slrs/fujifilm_xs10/specifications
-    url = BASE_URL + "/"+camera+"/specifications"
-    page = requests.get(url)
+    url = camera_url+"/specifications"
+    print("Obteniendo especificaciones..."+url)
+    # page = s.get(url)
     # Parseo de HTML utilizando beautifulsoup
-
     return specs
 
 
-def get_review(camera):
+def get_review(camera_url):
     # camera: fujifilm/slrs/fujifilm_xs1
     # Miki
     # https://www.dpreview.com/products/fujifilm/slrs/fujifilm_xs10/review
     review = {}
-    url = BASE_URL + "/"+camera+"/review"
-    page = requests.get(url)
+    url = camera_url+"/review"
+    print("Obteniendo reviews..."+url)
+    # page = s.get(url)
     # Parseo de HTML utilizando beautifulsoup
 
     return review
@@ -45,29 +96,12 @@ def save_data(camera_data, output_file):
 
 
 def main():
-    camera_list = get_camera_list()
+    camera_list = get_all_cameras()
     for camera in camera_list:
+        specs = get_specs(camera.get('link'))
+        if camera.get('review_link'):
+            review = get_review(camera.get('link'))
 
 
 if __name__ == "__main__":
     main()
-
-page = requests.get(BASE_URL)
-
-#soup = BeautifulSoup(page.content)
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
-years = soup.select(
-    "combinedProductList>div>table>thead:nth-child(1)>tr>th")
-
-#years = soup.find_all("tr", {"class": "groupLabel"})
-# combinedProductList > div > table > thead:nth-child(1) > tr > th
-# https://blog.jonmassey.co.uk/posts/chrome-beautifulsoup-scraping/
-# https://stackoverflow.com/questions/58136906/how-do-i-convert-css-selector-path-copied-from-chrome-to-beautifulsoup-obejct
-
-productList = soup.select("table.productList")
-print(productList)
-# for year in productList.select()
-# for year_group in soup.select("tr.productList"):
-#    year = year_group.select("tr>th")
-#    print(year)
